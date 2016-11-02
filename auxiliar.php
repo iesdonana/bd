e9917185539db4a3aa6133d7b4164c0dfcf6699a
Ricardo Pérez <ricpelo@gmail.com>
2016-11-02 20:19:22 +0100

    Carga inicial

---
+++ auxiliar.php
@@ -0,0 +1,213 @@
+<?php
+
+function exception_error_handler($severidad, $mensaje, $fichero, $línea) {
+    if (!(error_reporting() & $severidad)) {
+        // Este código de error no está incluido en error_reporting
+        return;
+    }
+    throw new ErrorException($mensaje, 0, $severidad, $fichero, $línea);
+}
+set_error_handler("exception_error_handler");
+
+/**
+ * Muestra por la salida los errores del argumento.
+ * @param  array $err El array que contiene los errores
+ */
+function mostrar_errores($err)
+{
+    foreach ($err as $e) { ?>
+        <h3>Error: <?= htmlentities($e) ?></h3><?php
+    }
+}
+
+/**
+ * Muestra un mensaje de saludo.
+ * @param  string $nombre   El nombre de la persona
+ * @param  string $telefono El teléfono de la persona
+ */
+function saludar(string $nombre, string $telefono)
+{ ?>
+    <h2>Hola, <?= htmlentities($nombre) ?>.
+        Tu teléfono es <?= htmlentities($telefono) ?></h2><?php
+}
+
+function param_falta($param, $humano, &$error)
+{
+    if ($param === null) {
+        $error[] = "Falta el campo $humano";
+        return true;
+    }
+    return false;
+}
+
+function param_longmax($param, $humano, $max, &$error)
+{
+    if (mb_strlen($param) > $max) {
+        $error[] = "El campo $humano no puede superar los $max caracteres";
+        return true;
+    }
+    return false;
+}
+
+function param_vacio($param, $humano, &$error)
+{
+    if ($param === "") {
+        $error[] = "El campo $humano no puede estar vacío";
+        return true;
+    }
+    return false;
+}
+
+function comprobar_nombre($nombre, $humano, &$error)
+{
+    if (param_falta($nombre, $humano, $error) ||
+        param_longmax($nombre, $humano, 50, $error) ||
+        param_vacio($nombre, $humano, $error)) {
+        return;
+    }
+}
+
+function comprobar_telefono($telefono, $humano, &$error)
+{
+    if (param_falta($telefono, $humano, $error) ||
+        param_longmax($telefono, $humano, 9, $error)) {
+        return;
+    }
+
+    if (filter_var($telefono, FILTER_VALIDATE_INT, [
+        'options' => [
+            'min_range' => 100000000,
+            'max_range' => 999999999
+        ]
+    ]) === false) {
+        $error[] = "El campo $humano debe ser un número de 9 dígitos";
+    }
+}
+
+function comprobar_errores($error)
+{
+    if (!empty($error)) {
+        throw new Exception;
+    }
+}
+
+function comprobar_existen($params)
+{
+    foreach ($params as $p) {
+        if ($p !== null) {
+            return true;
+        }
+    }
+    throw new Exception;
+}
+
+function comprobar_dept_no(&$dept_no, array &$error)
+{
+    if ($dept_no === null) {
+        throw new Exception;
+    }
+
+    $dept_no = trim($dept_no);
+
+    if ($dept_no !== "" && !ctype_digit($dept_no)) {
+        $error[] = "El número de departamento debe ser un número";
+    }
+
+    if (mb_strlen($dept_no) > 2) {
+        $error[] = "El número de departamento debe contener 1 ó 2 dígitos";
+    }
+}
+
+function comprobar_dnombre(&$dnombre, array &$error)
+{
+    if ($dnombre === null) {
+        throw new Exception;
+    }
+
+    $dnombre = trim($dnombre);
+
+    if (mb_strlen($dnombre) > 20) {
+        $error[] = "El nombre del departamento no puede tener más de 20 caracteres";
+    }
+}
+
+function comprobar_si_vacio(array $result, array &$error)
+{
+    if (empty($result)) {
+        $error[] = "No existe ese departamento";
+    }
+}
+
+function comprobar_si_hay_uno(array $params, array &$error): bool
+{
+    foreach ($params as $p) {
+        if ($p !== "") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function conectar_bd(): PDO
+{
+    return new PDO(
+        'pgsql:host=localhost;dbname=prueba',
+        'ricardo',
+        'ricardo'
+    );
+}
+
+function buscar_por_dept_no(PDO $pdo, string $dept_no): array
+{
+    $orden = $pdo->prepare("select *
+                              from depart
+                             where dept_no = :dept_no");
+    $orden->execute([':dept_no' => $dept_no]);
+    return $orden->fetchAll();
+}
+
+function buscar_por_dept_no_y_dnombre(
+    PDO $pdo,
+    string $dept_no,
+    string $dnombre
+): array {
+    if ($dept_no === "") {
+        $orden = $pdo->prepare("select *
+                                  from depart
+                                 where dnombre like :dnombre");
+        $orden->execute([':dnombre' => "%$dnombre%"]);
+    } elseif ($dnombre === "") {
+        $orden = $pdo->prepare("select *
+                                  from depart
+                                 where dept_no = :dept_no");
+        $orden->execute([':dept_no' => $dept_no]);
+    } else {
+        $orden = $pdo->prepare("select *
+                                  from depart
+                                 where dept_no = :dept_no and
+                                       dnombre like :dnombre");
+        $orden->execute([':dept_no' => $dept_no, ':dnombre' => "%$dnombre%"]);
+    }
+
+    return $orden->fetchAll();
+}
+
+function dibujar_tabla(array $result)
+{ ?>
+    <table border="1">
+        <thead>
+            <th>DEPT_NO</th>
+            <th>DNOMBRE</th>
+            <th>LOC</th>
+        </thead>
+        <tbody><?php
+            foreach ($result as $fila) { ?>
+                <tr>
+                    <td><?= $fila['dept_no'] ?></td>
+                    <td><?= $fila['dnombre'] ?></td>
+                    <td><?= $fila['loc'] ?></td>
+                </tr><?php
+            } ?>
+        </tbody>
+    </table><?php
+}
---
+++ bd.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bases de datos</title>
+    </head>
+    <body><?php
+        require 'auxiliar.php'; ?>
+
+        <form action="" method="post">
+            <label for="dept_no">Número de departamento:</label>
+            <input type="text" id="dept_no" name="dept_no" /><br/>
+            <label for="dnombre">Nombre de departamento:</label>
+            <input type="text" id="dnombre" name="dnombre" /><br/>
+            <input type="submit" value="Buscar" />
+        </form><?php
+
+        try {
+            $dept_no = filter_input(INPUT_POST, "dept_no");
+            $dnombre = filter_input(INPUT_POST, "dnombre");
+            $error = [];
+            comprobar_dept_no($dept_no, $error);
+            comprobar_dnombre($dnombre, $error);
+            comprobar_si_hay_uno([$dept_no, $dnombre], $error);
+            comprobar_errores($error);
+            $pdo = conectar_bd();
+            $result = buscar_por_dept_no_y_dnombre($pdo, $dept_no, $dnombre);
+            comprobar_si_vacio($result, $error);
+            comprobar_errores($error);
+            dibujar_tabla($result);
+        } catch (PDOException $e) { ?>
+            <h3>Error de conexión a la base de datos</h3><?php
+        } catch (Exception $e) {
+            mostrar_errores($error);
+        } ?>
+    </body>
+</html>
