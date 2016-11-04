762b62db8e0c12e0ac102ff525404e2b8611570f
Christian Hidalgo <christianhf.chf@gmail.com>
2016-11-04 17:47:03 +0100

    Modificación de la base de datos

--- bd.sql
+++ bd.sql
@@ -1,7 +1,34 @@
+drop table if exists localidades cascade;
+
+create table localidades(
+    id        bigserial constraint pk_localidades primary key,
+    loc varchar(100) not null
+);
+
+insert into localidades (localidad)
+    values  ('SANLÚCAR'),
+            ('JEREZ'),
+            ('TREBUJENA'),
+            ('CHIPIONA');
+
 drop table if exists depart cascade;
 
 create table depart (
-    dept_no numeric(2)  constraint pk_depart primary key,
-    dnombre varchar(20) not null,
-    loc     varchar(50)
+    dept_no       numeric(2)  constraint pk_depart primary key,
+    dnombre       varchar(20) not null,
+    localidad_id  bigint      constraint fk_depart_localidades
+                              references localidades (id)
+                              on delete set null on update cascade
 );
+
+insert into (depart)
+    values  (10, 'CONTABILIDAD' , 1),
+            (20, 'INVESTIGACIÓN', 2),
+            (30, 'VENTAS', 3),
+            (40, 'PRODUCCIÓN', 4);
+
+drop view if exists depaart_v cascade;
+
+create view depart_v as
+    select *
+    from depart d join localidades l on d.localidad_id = l.id;
